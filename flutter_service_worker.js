216462a1e37b6ea3c6143090c1c5e007a303b8da
Carlos Schellenberger <carlos.schellenberger@gmail.com>
2021-07-10 16:21:46 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "8002ee6afad7fc3f1d7cd80ce6ed5ae1",
-"/": "8002ee6afad7fc3f1d7cd80ce6ed5ae1",
-"main.dart.js": "19571adcba6577008ddb25f3cfed34ba",
+"index.html": "1f1b801afe8e904368c0cc4e8ab3e723",
+"/": "1f1b801afe8e904368c0cc4e8ab3e723",
+"main.dart.js": "959fd4df134ccb5f6d40f0f141dc5efd",
 "manifest.json": "d9cd7826ca19fe627866e1dc86f0642c",
 "version.json": "a6bfa4f02e90dbea213e2f1f240743f5"
 };
